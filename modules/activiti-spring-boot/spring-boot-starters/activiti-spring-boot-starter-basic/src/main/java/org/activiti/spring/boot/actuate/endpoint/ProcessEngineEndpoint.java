b18e74ff698b6089904ad9e3ddb83dcaa8dc6d42
我叫不高兴 <13662138552@163.com>
2019-08-08 17:26:39 +0800

    完成activiti-boot-starers-basics升级springboot2.1.4

--- modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/actuate/endpoint/ProcessEngineEndpoint.java
+++ modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/actuate/endpoint/ProcessEngineEndpoint.java
@@ -17,10 +17,8 @@ import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.activiti.engine.impl.persistence.deploy.DefaultDeploymentCache;
 import org.activiti.engine.impl.persistence.deploy.DeploymentCache;
 import org.activiti.engine.impl.persistence.deploy.ProcessDefinitionCacheEntry;
-import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.repository.ProcessDefinition;
-import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 
 import java.util.*;
 
@@ -30,18 +28,20 @@ import java.util.*;
  *
  * @author Josh Long
  */
-@ConfigurationProperties(prefix = "endpoints.activiti")
-public class ProcessEngineEndpoint extends AbstractEndpoint<Map<String, Object>> {
+//@ConfigurationProperties(prefix = "endpoints.activiti")
+
+@Endpoint(id = "activiti")
+public class ProcessEngineEndpoint {
 
     private final ProcessEngine processEngine;
 
     public ProcessEngineEndpoint(ProcessEngine processEngine) {
-        super("activiti");
+//        super("activiti");
         this.processEngine = processEngine;
     }
 
-    @Override
-    public Map<String, Object> invoke() {
+//    @Override
+    public Map<String, Object> activiti() {
 
         Map<String, Object> metrics = new HashMap<String, Object>();
 
